8e7f64290b8aa9493a5f49339c5abe48158c3217
sanjeev0303 <sanjeev.081021@gmail.com>
2024-08-11 20:08:47 +0530

    add MultiplicationTable

---
+++ Practice Qestions/1.MultiplicationTable/MultiplicationTable.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Multiplication Table Wrold!");
+        System.out.println("Please enter any number: ");
+        int num = scanner.nextInt();
+        printMultiplicationTable(num);
+        scanner.close();
+    }
+
+    public static void printMultiplicationTable(int num) {
+        int i = 1;  
+
+        while (i <= 10) {
+            System.out.println(num + " X " + i + " = " + (num * i));
+            i++;
+        }
+    }
+
+}
